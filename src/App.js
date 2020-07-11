09311b03ef82f1a545b5b623f4be14b579bb4c66
ChoiPuReum <poo1994@hanmail.net>
2020-07-11 19:04:38 +0900

    Create ReactProject

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>let's plkay!</h2>
+    </div>
+  );
+}
+
+export default App;
