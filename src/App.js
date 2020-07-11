ed51f92db75434dbb46dec2a1d1d8d969a793ea7
ChoiPuReum <poo1994@hanmail.net>
2020-07-11 21:55:55 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>let's plkay!</h2>
+    <div>
+      {
+        customers.map(c =>{
+          return(
+            <Customer
+              //map을 사용할때는 key를 설정해줘야함(primarykey)
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+          );
+
+        })
+      }  
     </div>
   );
+}
+const customers=[{
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'최푸름',
+  'birthday':'940418',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'최상옥',
+  'birthday':'960419',
+  'gender':'여자',
+  'job':'대학생'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'김민철',
+  'birthday':'9103111',
+  'gender':'남자',
+  'job':'프로그래머'
+}
+]
+
+function Customer(props){
+  return(
+    <div>
+       <CustomerProfile id={props.id} image={props.image} name={props.name}/>
+       <CustomerInfo birthday={props.birthday} gender={props.gender} job={props.job}/>      
+    </div>
+  ) 
+}
+
+function CustomerProfile(props){
+  return(
+    <div>
+      <img src={props.image} alt='profile'/>
+      <h2>{props.name}({props.id})</h2>
+    </div>
+  )
+}
+function CustomerInfo(props){
+  return(
+    <div>
+      <p>{props.birthday}</p>
+      <p>{props.gender}</p>
+      <p>{props.job}</p>
+
+    </div>
+  )
 }
 
 export default App;
