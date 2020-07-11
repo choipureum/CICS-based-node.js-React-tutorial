d14736969eb8bf132a476b3b483096feb416a44b
ChoiPuReum <poo1994@hanmail.net>
2020-07-11 22:28:49 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme =>({
+  root:{
+    width:'100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "scroll"
+  },
+  table:{
+    minWidth:1080
+  }
+ 
+})
 
 function App(props) {
+  const {classes} =props
   return (
-    <div>
-      {
-        customers.map(c =>{
-          return(
-            <Customer
+    <Paper className={classes.root}>
+      <Table className={classes.talbe}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+        </TableHead>
+        <TableBody>
+      {customers.map(c =>{ return( <Customer
               //map을 사용할때는 key를 설정해줘야함(primarykey)
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              />
-          );
-
-        })
-      }  
-    </div>
+              key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}  
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 const customers=[{
@@ -52,19 +70,14 @@ const customers=[{
 
 function Customer(props){
   return(
-    <div>
-       <CustomerProfile id={props.id} image={props.image} name={props.name}/>
-       <CustomerInfo birthday={props.birthday} gender={props.gender} job={props.job}/>      
-    </div>
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell><img src={props.image} alt='profile'></img></TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthday}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow> 
   ) 
 }
-
-function CustomerProfile(props){
-  return(
-    <div>
-      <img src={props.image} alt='profile'/>
-      <h2>{props.name}({props.id})</h2>
-    </div>
-  )
-}
-export default App;
+export default withStyles(styles)(App);
